c37b00eee6584db10e8094aa20f706b2379cb174
Alex Zeffertt <alex.zeffertt@gmail.com>
2022-04-14 17:12:00 +0100

    basic social graph complete

--- code/graph.py
+++ code/graph.py
@@ -3,10 +3,9 @@
 # Build a social network graph representative of individuals in North Jutland, Denmark.
 # The edges represent social ties along which influence over dietary habits may pass.
 # 
-# The graph is a networkx.Graph, i.e. an undirected graph without multiple edges between nodes.
-# Edges have two weights, a "strong tie" weight which is reserved for cohabitant ties; and a
-# "weak tie" weight, reserved for non-cohabitants with which individuals frequently eat (>once
-# per month).
+# The graph is a undirected graph without multiple edges between nodes.
+# Edges are either "strong ties" i.e. cohabitant ties; or "weak ties", i.e. non-cohabitants
+# who eat together frequently (>once per month). 
 #
 # The distribution of households for each household size are assumed to match that for Denmark
 # as a whole and the distribution for Denmark for Jan 2022 from statistica.com is used.  Each
@@ -15,22 +14,38 @@
 # placed between every pair of cohabitants.
 #
 # We shall assume that the number of weak ties conforms to a Poisson distribution, which is
-# determined entirely its mean.  Since weak ties correspond to individuals which regularly dine
+# determined entirely by mean_n_weak_ties.  Since weak ties correspond to individuals which regularly dine
 # together (but do not cohabit) it is reasonable to assume that the physical distance 
 # distance between them is determined by a distribution with mean 0.  We shall use a normal
-# distribution, which is therefore characterized by standard_deviation sigma alone.
+# distribution, which is therefore characterized by standard_deviation weak_tie_sigma_km.
+#
+# The final graph is represented as a tuple
+#
+#     (coords, edges, strong_tie)
+#
+# where coords is an Nx2 numpy array of longitude and latitude, edges is a Ex2 array of
+# indices into coords, and strong_tie is an 1xE array of bools.  The tuple is saved in the file
+#
+#     ./NorthJutlandSocialGraph_{mean_n_weak_ties}_{weak_tie_sigma_km}.pickle
+#
+# Usage:
+#
+# Run this file with the command line
+#
+#     ./graph.py <mean_n_weak_ties>_<weak_tie_sigma_km>
 
 import pickle
-import networkx as nx
 import numpy as np
 import sys
 
-###### Configuration parameters #######
-strong_tie_weight = 1.0
-weak_tie_weight = 0.5
-mean_n_weak_ties  = 3   if len(sys.argv) < 2 else int(sys.argv[1])
-weak_tie_sigma_km = 1.0 if len(sys.argv) < 3 else float(sys.argv[1])
+###### Configuration parameters ########
+# NB: population_thinning_factor is a debug option to speed things up
+mean_n_weak_ties  = 6   if len(sys.argv) < 2 else int(sys.argv[1])
+weak_tie_sigma_km = 1.0 if len(sys.argv) < 3 else float(sys.argv[2])
+population_thinning_factor = 1  if len(sys.argv) < 4 else float(sys.argv[3])
 
+print(f"mean_n_weak_ties={mean_n_weak_ties} "
+      f"weak_tie_sigma_km={weak_tie_sigma_km}")
 ###### Datasets which we need to build a representative graph #######
 
 # Read in population density data per km2 (z_grid)
@@ -47,34 +62,37 @@ dy_deg = y_grid[0,1]-y_grid[0,0]
 dx_km = (np.pi*dx_deg/180)*R_km*np.cos(np.pi*y_grid[0,0]/180)
 dy_km = (np.pi*dy_deg/180)*R_km
 dA_km2 = dx_km*dy_km
-n_pop_NJ = int(z_grid.sum()*dA_km2)
+n_pop_NJ = int(z_grid.sum()*dA_km2/population_thinning_factor)
 
 # Data for Jan 2022 from
 # https://www.statista.com/statistics/582641/households-by-household-size-in-denmark/
 # NB: In the original data the last line is "8 or more" but we are going to
 # truncate
-n_households_by_size_DK = { 1: 1107109, 2: 929014, 3: 315346, 4: 299579,
-                            5: 102080,  6: 23200,  7: 6560,   8: 5403}
-
+n_households_by_size_DK = {
+    1: 1107109, 2: 929014, 3: 315346, 4: 299579,
+    5: 102080,  6: 23200,  7: 6560,   8: 5403
+}
 # Calculate equivalent for North Jutland, assuming the proportions are similar
 n_pop_DK = sum([ k*v for k,v in n_households_by_size_DK.items() ])
-n_households_by_size_NJ = { k: round(v*n_pop_NJ/n_pop_DK)
-                            for k,v in n_households_by_size_DK.items() }
+n_households_by_size_NJ = { k: round(v*n_pop_NJ/n_pop_DK) for k,v in n_households_by_size_DK.items() }
+n_nodes = sum([ k*v for k,v in n_households_by_size_NJ.items() ])
 
-# Create a undirected graph in which each node is a person
-# indexed by their position (in degrees longitude and latitude).
-# edges are strong or weak ties.  Strong is interpreted as co-habitation
-g = nx.Graph()
+# edge_dict is a lookup (idx1,idx2) -> bool(strong_tie) where idx1>idx2 and will be converted later on to edges
+coords = np.ndarray((n_nodes, 2), dtype=float)
+idxs = np.arange(n_nodes)
+edge_dict = {}
+
 
 # Add the strong ties by placing households at random according to pop Density
 m,n = z_grid.shape
-idx = np.arange(m*n, dtype=int)
+grid_idx = np.arange(m*n, dtype=int)
 probs = z_grid.flatten() / z_grid.sum()
 
+idx = 0
 for k,v in n_households_by_size_NJ.items():
     household_sz = k
     n_households = v
-    household_idx = np.random.choice(idx, n_households, p=probs)
+    household_idx = np.random.choice(grid_idx, n_households, p=probs)
     household_x_idx = household_idx//n
     household_y_idx = household_idx%n
     jitter_x = np.random.normal(0,dx_deg/3,size=n_households)
@@ -84,18 +102,13 @@ for k,v in n_households_by_size_NJ.items():
     # house radius in km
     r_km = 0.02
     for x,y in zip(household_x,household_y):
-        nodes = []
-        weighted_edges = []
         for i in range(household_sz):
             x_node = x + r_km*dx_deg/dx_km * np.cos(2*np.pi*i/household_sz)
             y_node = y + r_km*dy_deg/dy_km * np.sin(2*np.pi*i/household_sz)
-            nodes.append((x_node,y_node))
-        for i in range(1,len(nodes)):
+            coords[idx] = (x_node,y_node)
             for j in range(i):
-                weighted_edges.append((nodes[i],nodes[j],strong_tie_weight))
-        g.add_nodes_from(nodes)
-        g.add_weighted_edges_from(weighted_edges)
-
+                edge_dict[idx,idx-i+j] = True # strong tie
+            idx += 1
 
 # Add random links between households
 
@@ -108,21 +121,19 @@ x_blocklims = np.linspace(x_min-epsilon,x_max+epsilon,round(x_range/sigma_x))
 y_blocklims = np.linspace(y_min-epsilon,y_max+epsilon,round(y_range/sigma_y))
 
 # Choose a number of weak ties for each node
-nodes = np.array(g.nodes)
-node_idx = np.arange(len(nodes),dtype=int)
-n_weak_ties = np.random.poisson(lam=mean_n_weak_ties,size=len(nodes))
+# NB: divide by 2 to account for fact this node can be a target as well as source
+n_weak_ties = np.random.poisson(lam=mean_n_weak_ties/2,size=n_nodes)
 max_n_weak_ties = max(n_weak_ties[:])
 
 for i in range(len(x_blocklims)-1):
     x_start, x_end = x_blocklims[i], x_blocklims[i+1]
     x_midpt = (x_start + x_end)/2
-    print(f"mean_n_weak_ties={mean_n_weak_ties} weak_tie_sigma_km={weak_tie_sigma_km} {i}/{len(x_blocklims)}")
     for j in range(len(y_blocklims)-1):
         y_start, y_end = y_blocklims[j], y_blocklims[j+1]
         y_midpt = (y_start + y_end)/2
         
         # create probabilities for making links to each node in NJ using normal distrib
-        delta_deg = nodes - np.array((x_midpt,y_midpt))
+        delta_deg = coords - np.array((x_midpt,y_midpt))
         delta_km = delta_deg * np.array((dx_km/dx_deg, dy_km/dy_deg))
         dists_km = np.sqrt(delta_km[:,0]**2 + delta_km[:,1]**2)
         if dists_km.min() > 6*sigma_km:
@@ -132,19 +143,47 @@ for i in range(len(x_blocklims)-1):
 
         # subset of nodes in this block
         inblock = \
-            (x_start <= nodes[:,0]) & (nodes[:,0] < x_end) & \
-            (y_start <= nodes[:,1]) & (nodes[:,1] < y_end)
-        node_idx_inblock = node_idx[inblock]
-        n_inblock = len(node_idx_inblock)
+            (x_start <= coords[:,0]) & (coords[:,0] < x_end) & \
+            (y_start <= coords[:,1]) & (coords[:,1] < y_end)
+        inblock_idxs = idxs[inblock]
+        n_inblock = len(inblock_idxs)
 
-        weighted_edges = []
-        peer_idxs = np.random.choice(node_idx,p=prob,size=(n_inblock,max_n_weak_ties))
-        for idx, _peer_idxs, n_peers in zip(node_idx_inblock, peer_idxs, n_weak_ties[node_idx_inblock]):
-            weighted_edges.extend([
-                (tuple(nodes[idx]),tuple(nodes[peer_idx]),weak_tie_weight)
-                for peer_idx in _peer_idxs[:n_peers] if peer_idx != idx
-            ])
-        g.add_weighted_edges_from(weighted_edges)
+        # Randomly choose peers for each node in inblock_idxs
+        peer_idxs = np.random.choice(idxs, p=prob, size=(n_inblock,max_n_weak_ties))
+        for idx, _peer_idxs, n_peers in zip(inblock_idxs, peer_idxs, n_weak_ties[inblock_idxs]):
+            for peer_idx in _peer_idxs[:n_peers]:
+                if peer_idx != idx:
+                    lhs = max(idx, peer_idx)
+                    rhs = min(idx, peer_idx)
+                    edge = (lhs,rhs)
+                    if edge not in edge_dict:
+                        edge_dict[edge] = False # weak tie
+
+        percent_done = ((i+1)*len(y_blocklims) + (j+1))*100
+        percent_done /= (len(x_blocklims)*len(y_blocklims))
+        print(f"\r{percent_done:.1f}%", end="")
 
 # Save graph
-open(f'NorthJutlandSocialGraph_{mean_n_weak_ties}_{weak_tie_sigma_km}.pickle','wb').write(pickle.dumps(g))
+edges = np.array(list(edge_dict.keys()), dtype=int)
+strong_tie = np.array([ edge_dict[i,j] for (i,j) in edges ], dtype=bool)
+
+open(f'NorthJutlandSocialGraph_{mean_n_weak_ties}_{weak_tie_sigma_km}.pickle','wb').write(
+    pickle.dumps((coords, edges, strong_tie))
+)
+
+
+if 0:
+    # plot just the strong ties
+    import NorthJutlandPopDensity
+    import matplotlib.pyplot as plt
+    from matplotlib import collections as mc
+    NorthJutlandPopDensity.plot()
+    plt.ioff()
+    ax = plt.gca()
+    plt.scatter(coords[:,0],coords[:,1],marker='.',
+                linewidths=1,color='black')
+    ax.add_collection(mc.LineCollection(coords[edges[strong_tie]],
+                                        linewidths=1, colors='black'))
+    ax.add_collection(mc.LineCollection(coords[edges[strong_tie==False]],
+                                        linewidths=1, colors='black', alpha=0.5))
+    plt.show()
