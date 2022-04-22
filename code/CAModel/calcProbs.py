3f83578679ce8247f20aa06cdbb9c24a0bdf481a
Alex Zeffertt <alex.zeffertt@gmail.com>
2022-04-22 17:02:39 +0100

    CAModel/calcProbs.py now produces table showing what "awareness" and "facility" indicate

--- code/CAModel/calcProbs.py
+++ code/CAModel/calcProbs.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+#
+# Experimentation with the results from H&L
+# to try and reverse their published data
+# and produce probabilities that can be used in CAModel.py
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -97,7 +102,6 @@ for percent, ax in zip(percent_splits, axes.flatten()):
 
     # This allows us to work out
     P = np.exp(C + beta)/np.exp(C + beta).sum(axis=0)
-    print(P)
 
     # Plot the result
     plt.sca(ax)
@@ -115,11 +119,40 @@ axes[1][1].set_xlabel('Number of meat-reducer social-ties')
 axes[0][0].set_ylabel('Probability')
 axes[1][0].set_ylabel('Probability')
 
-# NB: We know from the M&L paper that in the Danish population as a whole we have the following split
+# NB: We know from the H&L paper that in the Danish population as a whole we have the following split
 #
 #  NO intention    Intention    Reducer
 #  57.3%           11.4%        31.2%
 #
 # This generates an upper bound for a realistic C_1 and C_2, namely log(11.4/57.3) and log(31.2/57.3)
 # Since the ratios must be smaller if we restrict to just the population that have no ties to meat-reducers.
+
+# Let's transform C_1 and C_2 into two more easily characterized variables
+# 1. Awareness
+# 
+# This is the percentage of the no-ties population that are
+# either Intending to reduce or actively reducing meat consumption.  We'll call
+# this the societal awareness because it can only depend on global factors such
+# as climate campaigns, and not information from meat-reducer peers.
+#
+#    Awareness := 100 * (no_ties_intention + no_ties_reducer) / (no_ties_all)
+#
+# 2. Facility
+#
+# This is the percentage of the "aware" no-ties population that are active
+# reducers.  This represents how easy society as a whole makes being a meat
+# reducer, and includes availability of ingredients and catered options,
+# skills and information, motivation
+#
+#    Facility := 100 * (no_ties_reducer) / (no_ties_intention + no_ties_reducer)
+
+
+print(f"{'NO intent':>10s}% {'Intention':>10s}% {'Reducer':>10s}% {'awareness':>10s}%  {'facility':>10s}%")
+for percent in percent_splits:
+    awareness = percent[1] + percent[2]
+    facility = 100 * percent[2] / awareness
+    print(f"{percent[0]:10d}% {percent[1]:10d}% {percent[2]:10d}% {awareness:10.1f}%  {facility:10.1f}%")
+
+
+
 
